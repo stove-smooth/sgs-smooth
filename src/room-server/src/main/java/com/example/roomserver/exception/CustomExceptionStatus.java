24163412bdd3aceffc8e606cbd9f3b350ce81424
Heedong Kim <ruthetum@gmail.com>
2021-12-30 13:25:45 +0900

    [feat] room server 예외 처리 설정

---
+++ src/room-server/src/main/java/com/example/roomserver/exception/CustomExceptionStatus.java
@@ -0,0 +1,20 @@
+package com.example.roomserver.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum CustomExceptionStatus {
+
+    SUCCESS(true, 1000, "요청에 성공하였습니다."),
+    BAD_REQUEST(false, 1400, "잘못된 요청입니다."),
+    INTERNAL_SERVER_ERROR(false, 1500, "서버 내부 에러입니다.");
+
+    // 3000 ~ 3999
+
+
+    private final boolean isSuccess;
+    private final int code;
+    private final String message;
+}
