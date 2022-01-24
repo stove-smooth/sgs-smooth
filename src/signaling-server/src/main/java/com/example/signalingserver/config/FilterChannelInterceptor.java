d612c32581dd325f175ea86b74495c68f3f4bc53
Heedong Kim <ruthetum@gmail.com>
2022-01-24 12:09:52 +0900

    [feat] 시그널링 서버 ChannelInterceptor, token filter 추가

---
+++ src/signaling-server/src/main/java/com/example/signalingserver/config/FilterChannelInterceptor.java
@@ -0,0 +1,59 @@
+package com.example.signalingserver.config;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.stereotype.Component;
+
+import java.util.Objects;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class FilterChannelInterceptor implements ChannelInterceptor {
+
+    private final JwtTokenFilter jwtTokenFilter;
+    private final RedisTemplate redisTemplate;
+
+    private static final String ACCESS_TOKEN = "access-token";
+    private static final String USER_ID = "user-id";
+    private static final String CHANNEL_ID = "channel-id";
+
+    // TODO JWT 검증
+    @Override
+    public Message<?> preSend(Message<?> message, MessageChannel channel) {
+//        StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
+//        if (CONNECT.equals(accessor.getCommand())) {
+//            if (!jwtTokenFilter.isJwtValid(Objects.requireNonNull(accessor.getFirstNativeHeader(ACCESS_TOKEN)))) {
+//                log.info("실패");
+//                throw new ResponseStatusException(HttpStatus.UNAUTHORIZED);
+//            }
+//        }
+        return message;
+    }
+
+    @Override
+    public void postSend(Message<?> message, MessageChannel channel, boolean sent) {
+        StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
+        switch (accessor.getCommand()) {
+            case CONNECT:
+                String connectSessionId = accessor.getSessionId();
+                String connectUserId = Objects.requireNonNull(accessor.getFirstNativeHeader(USER_ID));
+                String connectChannelId = Objects.requireNonNull(accessor.getFirstNativeHeader(CHANNEL_ID));
+                break;
+            case DISCONNECT:
+                String disconnectSessionId = accessor.getSessionId();
+                break;
+            default:
+//                throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+                break;
+        }
+    }
+
+}
+
