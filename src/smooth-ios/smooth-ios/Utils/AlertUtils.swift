8ecf7159d53770bad6db053a1001cc000a0d85c1
doitduri <kdle_@naver.com>
2022-02-03 14:45:18 +0900

    [feat] showAlert 추가

--- src/smooth-ios/smooth-ios/Utils/AlertUtils.swift
+++ src/smooth-ios/smooth-ios/Utils/AlertUtils.swift
@@ -66,4 +66,12 @@ struct AlertUtils {
       controller.present(alrtController, animated: true)
     }
     
+    static func showAlert(style: UIAlertController.Style, title: String?, message: String?, actions: [UIAlertAction] = [UIAlertAction(title: "Ok", style: .cancel, handler: nil)], completion: (() -> Swift.Void)? = nil) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: style)
+        for action in actions {
+            alert.addAction(action)
+        }
+        
+        UIApplication.shared.delegate?.window??.rootViewController?.present(alert, animated: true, completion: completion)
+    }
 }
